79c5b33a9430779c6d390fa060b2afd0c287dea6
NoeGarcia2022 <noegarcia4789@gmail.com>
2022-06-23 11:52:21 -0600

    Login

---
+++ src/java/Model/Empleado.java
@@ -0,0 +1,72 @@
+
+package Model;
+
+public class Empleado {
+    int id;
+    String dui;
+    String nom;
+    String tel;
+    String estado;
+    String user;
+
+    public Empleado() {
+    }
+
+    public Empleado(int id, String dui, String nom, String tel, String estado, String user) {
+        this.id = id;
+        this.dui = dui;
+        this.nom = nom;
+        this.tel = tel;
+        this.estado = estado;
+        this.user = user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDui() {
+        return dui;
+    }
+
+    public void setDui(String dui) {
+        this.dui = dui;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+    
+}
---
+++ src/java/Model/EmpleadoDAO.java
@@ -0,0 +1,16 @@
+
+package Model;
+
+import Factory.ConexionBD;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class EmpleadoDAO {
+    ConexionBD cn=new ConexionBD();
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    
+    
+}

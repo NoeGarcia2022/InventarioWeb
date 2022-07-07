6c6b9110809acc92fe1c8415e9e65002eabf9f45
Crespin2 <jonathan.molina21@itca.edu.sv>
2022-07-06 19:48:23 -0600

    Base de datos Final

--- recursos/bd_inventario.sql
+++ recursos/bd_inventario.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-06-2022 a las 20:23:20
--- Versión del servidor: 10.4.20-MariaDB
--- Versión de PHP: 8.0.8
+-- Tiempo de generación: 07-07-2022 a las 03:40:56
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,6 +26,35 @@ USE `bd_inventario`;
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `IdEmpleado` int(11) NOT NULL,
+  `dui` varchar(10) NOT NULL,
+  `Nombres` varchar(255) NOT NULL,
+  `Apellidos` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Telefono` varchar(9) NOT NULL,
+  `User` varchar(8) NOT NULL,
+  `Clave` varchar(15) NOT NULL,
+  `Tipo` int(3) NOT NULL,
+  `Estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `empleado`
+--
+
+INSERT INTO `empleado` (`IdEmpleado`, `dui`, `Nombres`, `Apellidos`, `Email`, `Telefono`, `User`, `Clave`, `Tipo`, `Estado`) VALUES
+(1, '06348599-2', 'Noe Antonio', 'Garcia Valle', 'noegarcia22@gmail.com', '7842-3952', 'admin1', '1234', 1, 1),
+(4, '3215453', 'jonathan', 'Molina', 'jona@email.com', '3515345', 'admin2', '12345', 1, 1),
+(5, '', '', ' ', '', '', '', '', 0, 0),
+(6, '', '', ' ', '', '', '', '', 0, 0);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `tb_categoria`
 --
 
@@ -40,9 +69,9 @@ CREATE TABLE `tb_categoria` (
 --
 
 INSERT INTO `tb_categoria` (`id_categoria`, `nom_categoria`, `estado_categoria`) VALUES
-(10, 'Queso', 1),
-(30, 'Laptop Lenovo', 1),
-(40, 'TV LG', 1);
+(1, 'Ropa', 1),
+(2, 'Calzado', 1),
+(3, 'Accesorios', 1);
 
 -- --------------------------------------------------------
 
@@ -53,16 +82,30 @@ INSERT INTO `tb_categoria` (`id_categoria`, `nom_categoria`, `estado_categoria`)
 CREATE TABLE `tb_producto` (
   `id_producto` int(9) NOT NULL,
   `nom_producto` varchar(50) NOT NULL,
-  `stock` decimal(3,2) DEFAULT NULL,
-  `precio` decimal(3,2) DEFAULT NULL,
+  `stock` decimal(10,2) DEFAULT NULL,
+  `precio` decimal(10,2) DEFAULT NULL,
   `unidad_de_medida` varchar(20) DEFAULT NULL,
   `estado_producto` tinyint(1) DEFAULT NULL,
-  `categoria` int(5) DEFAULT NULL
+  `id_categoria` int(5) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `tb_producto`
+--
+
+INSERT INTO `tb_producto` (`id_producto`, `nom_producto`, `stock`, `precio`, `unidad_de_medida`, `estado_producto`, `id_categoria`) VALUES
+(1, 'Cadena de oro 10 k', '10.00', '150.99', 'kilates', 1, 3),
+(2, 'Pumas talla 39', '8.00', '60.00', 'Plg', 1, 2);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`IdEmpleado`);
 
 --
 -- Indices de la tabla `tb_categoria`
@@ -75,23 +118,29 @@ ALTER TABLE `tb_categoria`
 --
 ALTER TABLE `tb_producto`
   ADD PRIMARY KEY (`id_producto`),
-  ADD KEY `categoria` (`categoria`);
+  ADD KEY `categoria` (`id_categoria`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `IdEmpleado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT de la tabla `tb_categoria`
 --
 ALTER TABLE `tb_categoria`
-  MODIFY `id_categoria` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id_categoria` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `tb_producto`
 --
 ALTER TABLE `tb_producto`
-  MODIFY `id_producto` int(9) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_producto` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
@@ -101,7 +150,7 @@ ALTER TABLE `tb_producto`
 -- Filtros para la tabla `tb_producto`
 --
 ALTER TABLE `tb_producto`
-  ADD CONSTRAINT `tb_producto_ibfk_1` FOREIGN KEY (`categoria`) REFERENCES `tb_categoria` (`id_categoria`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `tb_producto_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `tb_categoria` (`id_categoria`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
